03b956a95e73a9518750f4806a55c53802ec6198
Dan Minshew <ofenixculpa@gmail.com>
2019-02-09 17:55:42 -0600

    fix font bug

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,13 +2,15 @@ import Typography from "typography"
 
 const typography = new Typography({
   googleFonts: [{
-    name: 'Special Elite'
+    name: 'Special Elite',
+    styles: [
+      '400',
+    ],
   }],
   baseFontSize: '18px',
   baseLineHeight: 1.45,
   headerFontFamily: ['Special Elite', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
   bodyFontFamily: ['Comic Sans', 'serif'],
-  // See below for the full list of options.
 })
 
 export default typography
