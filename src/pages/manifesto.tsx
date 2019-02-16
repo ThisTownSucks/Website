0db0999203d357d152dd852db2ba8ca9fccfc1ad
Dan Minshew <ofenixculpa@gmail.com>
2019-02-16 15:47:51 -0600

    add manifesto page; update typography

--- src/pages/manifesto.tsx
+++ src/pages/manifesto.tsx
@@ -3,7 +3,68 @@ import Layout from '../components/layout'
 
 export default () => (
   <Layout>
-    <h1>Manifesto</h1>
-    <p>My cool appeal to power.</p>
+    <h2>This town… sucks.</h2>
+
+    <p>
+      The options of communication tools, social media platforms, and DIY
+      resources don’t cut it. What’s worse, they don’t address the particular
+      needs of artists, fans and venues.
+    </p>
+
+    <p>
+      Fans want to know where shows are happening. They want to know when bands
+      they like are coming to town. They want to be able to get their favorite
+      bands to play in their area. They want a meaningful way to express that
+      interest.
+    </p>
+
+    <p>
+      Bands want to know if this town will be a worthwhile stop. They want to
+      know if a venue is welcoming to their genre. They want to know who’s
+      available to open for them and the size and make-up of the crowd they can
+      expect.
+    </p>
+
+    <p>
+      Venues want to know which bands people are most interested in seeing. They
+      want to know what kind of turnout to expect. They want to know but about
+      the interests of the fans in their area and the make-up of the market in
+      which they’re embedded.
+    </p>
+
+    <p>
+      Historically, bands have had to make cold-calls, send instant messages to
+      strangers, beg to play a show in a town where they’ve never been, with no
+      guarantee that the person booking will put any effort toward promotion.
+      It’s often a loss for the band and the venue. Most bands make up for the
+      costs of touring by selling merchandise, but if the show is a bust,
+      they’re out a lot of money. It’s a huge stress on the band. A lot of bands
+      end up breaking up because of bad experiences on tour.
+    </p>
+
+    <p>
+      Likewise, fans get a generic interface with basic, manually entered, often
+      incomplete information about show times and venue location, and a vague
+      way to indicate interest in attending, only to never get a reminder from
+      the platform. That “I’m going” was just another dopamine hit for the event
+      creator, and not a meaningful exchange of intent. Bands and venues are
+      left disappointed when the real attendance didn’t match their
+      expectations.
+    </p>
+
+    <p>
+      Venues form around expectations of audiences that may or may not exist.
+      They often make business decisions based on incomplete knowledge. They
+      often rely on booking agents or agencies whose interests might not always
+      be aligned.
+    </p>
+
+    <p>
+      We think there’s an opportunity for venues to better serve the communities
+      they support, while also having more direct contact with the artists and
+      fans who compose it. There is more music to be made, more shows to be
+      played, and more fans to be reached. We want to give artists, venues, and
+      fans an unobscured view of the musical landscape around them.
+    </p>
   </Layout>
 )
