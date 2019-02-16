305964385de719f905cf6f24ef637ef9e383e910
Dan Minshew <ofenixculpa@gmail.com>
2019-02-16 17:02:07 -0600

    add formik; add bp questions component

---
+++ src/pages/questions.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout';
+
+const Questions = () => {
+  return <Layout>
+    <div>I'm some questions!</div>
+  </Layout>
+}
+
+export default Questions
